48bb1715154488e008e0dc2560a0a534278322eb
dewstouh <diegorodriguezpastor383@gmail.com>
2023-07-14 19:39:21 +0200

    ecosystem update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,19 @@
 const path = require('path');
-module.exports = {
-    apps: [
-        {
-            name: path.dirname(__filename).split(path.sep).pop(), //dir name
-            script: "index.js",
-            cron_restart: "0 0 * * *" // at 00:00
-        }
-    ]
+
+const splittedPath = path.dirname(__filename).split(path.sep);
+const parentFolder = splittedPath[splittedPath.length - 2];
+const folderName = splittedPath.pop();
+let processName = splittedPath.includes("HostedBots") ? `${parentFolder}_${folderName}` : folderName;
+function escapeText(text) {
+  return text.replace(/\s+/g, '_');
 }
+
+module.exports = {
+  apps: [
+    {
+      name: escapeText(processName),
+      script: "index.js",
+      cron_restart: "0 0 * * *" // at 00:00
+    }
+  ],
+};
